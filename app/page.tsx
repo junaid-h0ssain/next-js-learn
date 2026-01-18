8e53582a586bc92f94ab87f15fe0a3d4ccd13c95
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-01-19 02:23:55 +0600

    added global css with tw animate

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,12 @@ import Hello from '../components/hello';
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to My Next.js App</h1>
+      <h1>Welcome to My Next.js App. Now dance</h1>
       <Hello />
+
+      <a href="/about">
+        <button>go to about</button>
+      </a>
       
     </div>
   );
