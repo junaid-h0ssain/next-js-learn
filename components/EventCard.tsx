b54289a791cda0f5de415cd51a7f4e3bfb22fe9d
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-01-24 23:07:24 +0600

    added icons

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -17,8 +17,14 @@ const EventCard = ({title, image, slug, location, date, time, description}: Scri
   return (
     <Link href={`/events/${slug}`} id='event-card'>
       <Image src={image} alt={title} width={400} height={300} className='rounded-lg poster'/>
-      <p className='location'>{location}</p>
-      <p className='date-time'>{date} at {time}</p>
+      <div className='flex flex-row gap-2'>
+        <Image src="/icons/pin.svg" alt="Location" width={14} height={14} />
+        <p>{location}</p>
+      </div>
+      <div className='datetime'>
+        <Image src="/icons/calendar.svg" alt="Date" width={14} height={14} />
+        <p>{date} at {time}</p>
+      </div>
       <p className='description'>{description}</p>
       <h3 className='title mt-7 mb-7'>{title}</h3>
     </Link>
