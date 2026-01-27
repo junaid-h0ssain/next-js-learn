19d1e58abd9a7a8d0b640507f507fa3422c73840
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-01-27 22:05:29 +0600

    tried to add cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
 import connectDB from '@/lib/mongodb';
 import { Event } from '@/database';
 import { NextRequest, NextResponse } from 'next/server';
+import { v2 as cloudinary } from 'cloudinary';
+
 
 export async function POST(request: NextRequest) {
     try {
@@ -23,6 +25,28 @@ export async function POST(request: NextRequest) {
             agenda: typeof event.agenda === 'string' ? event.agenda.split(';').map((s: string) => s.trim()) : event.agenda,
             tags: typeof event.tags === 'string' ? event.tags.split(';').map((s: string) => s.trim()) : event.tags,
         };
+
+        const file = formData.get('image') as File;
+
+        if(!file){
+            return NextResponse.json({ error: 'Image file is required' }, { status: 400 });
+        }
+        console.log('Received file:', file.name, file.size, file.type);
+
+        const arrayBuffer = await file.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        const uploadResult = await new Promise<{ secure_url: string; public_id: string }>((resolve, reject) => {
+            cloudinary.uploader.upload_stream({resource_type: 'image'}, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result as { secure_url: string; public_id: string });
+                }
+            }).end(buffer);
+        });
+
+        event.image = (uploadResult as { secure_url: string; public_id: string }).secure_url;
 
         const createdEvent = await Event.create(processedEvent);
 
